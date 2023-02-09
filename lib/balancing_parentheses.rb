46b4446e107087b1a5ce8e558bf9657293ec3f99
Tim <timtran007@gmail.com>
2023-02-09 01:07:02 -0600

    commit tree 0ee62b8bb6808d65e87bec57afa477041e393b7b

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,15 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+
+    string.chars.each do |char|
+        if stack.peek == "(" && char == ")"
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+    stack.size
+end
